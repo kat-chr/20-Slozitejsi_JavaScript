0b2b480f59425e9d53e112e71ce7661871a5a920
Kateřina Chroncová <k.chroncova@gmail.com>
2022-08-02 10:12:21 +0200

    udelani ukolu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JavaScript funguje!");
+
+let vizualniHlavniOdstavec = document.querySelector(".hlavni-odstavec");
+vizualniHlavniOdstavec.textContent = "Domovska stranka";
+
+let visualniMojeFoto = document.querySelector(".moje-foto");
+visualniMojeFoto.src = "img/druhe-foto.jpeg";
+
+let visualVzdelani = document.querySelector(".vzdelani");
+visualVzdelani.textContent = "Skoly";
+
+let VisualPredstaveniSe = document.querySelector(".predstaveni-se");
+VisualPredstaveniSe.style.color = "blue";
+
+
+function pridejZvyrazneni() {
+    let visualniZvyrazneny = document.querySelector(".tlacitko1");
+    visualniZvyrazneny.classList.add("zvyrazneny");
+}
